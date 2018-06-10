2a4b177651fb79bfb3c5d44f19749592ae91592f
Yuriy Nemtsov <nemtsov@gmail.com>
2018-06-10 16:06:35 -0400

    test/boot: ensure only .js files are considered specs

--- test/boot.js
+++ test/boot.js
@@ -3,5 +3,5 @@ import Vue from 'vue';
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
-const testsContext = require.context('./specs', true);
+const testsContext = require.context('./specs', true, /\.js$/);
 testsContext.keys().forEach(testsContext);
